abc057b862e0d1dd0d8629badc974afed0f20002
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-05 12:15:36 -0500

    ¿Qué es el controlador, la vista y el modelo?

---
+++ CursoMVC-NET-Core-Solution/CursoMVC-NET-Core/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using CursoMVC_NET_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC_NET_Core.Controllers
+{
+    public class UserController : Controller
+    {
+        //CONTROLADOR: Intermediario entre los modelos y las vistas
+        public IActionResult Index()
+        {
+            User user = new User();
+            user.Nombre = "Juan";
+            user.Email = "Correo@gmail.com";
+            return View(user);
+        }
+    }
+}
---
+++ CursoMVC-NET-Core-Solution/CursoMVC-NET-Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC_NET_Core.Models
+{
+    //MODELO: Clase que sirve para la representacion de datos
+    public class User
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+    }
+}
